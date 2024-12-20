7e8f1735acb91ed15fda022fcc11509344dea941
biolancer <lseillier@ukaachen.de>
2024-12-20 10:57:09 +0100

    add missing module

---
+++ bin/sigprofiler_alt.py
@@ -0,0 +1,172 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Load dependencies
+"""
+
+import os
+import sys
+import shutil
+import pandas as pd
+import glob
+import argparse
+import logging
+from SigProfilerMatrixGenerator import install as genInstall
+from SigProfilerAssignment import Analyzer as sig
+
+"""
+CMD line parser
+"""
+
+def parse_args(argv=None):
+    """Define and immediately parse command line arguments."""
+    parser = argparse.ArgumentParser(
+        description="Full workflow package from SigProfiler which generates both matrices and the according Signature assignment.",
+        epilog="Example: NA",
+    )
+    parser.add_argument(
+        "--filetype",
+        type=str,
+        help="Defines the provided input filetype - GDC-MAF or a VCF-containing folder",
+        default="vcf"
+    )
+    parser.add_argument(
+        "--input",
+        type=str,
+        help="Path to input data folder",
+        default=""
+    )
+    parser.add_argument(
+        "--output_pattern",
+        type=str,
+        help="Identifier of the output files.",
+        default=""
+    )
+    parser.add_argument(
+        "--signature_database",
+        type=str,
+        help="Path to the alternative reference catalogue for analysis",
+        default=""
+    )
+    parser.add_argument(
+        "--ref",
+        type=str,
+        help="Reference genome from which the data was derived.",
+        default="GRCh38"
+    )
+    parser.add_argument(
+        "--context",
+        type=str,
+        help="Defines which context needs to be extracted, can be an SBS context, DINUC or ID or multiple ones separated by a comma",
+        default="96"
+    )
+    parser.add_argument(
+        "--l",
+        help="The desired log level (default INFO).",
+        choices=("CRITICAL", "ERROR", "WARNING", "INFO", "DEBUG"),
+        default="INFO",
+    )
+    return parser.parse_args(argv)
+
+############################################################################################################
+
+######################################################### Define analysis functions - MAF case
+
+### Function to check if the minimal amount of columns are present in the input MAF to be converted to Sigprofiler format
+def maf_formattest(maf_totest):
+    gdc_format_header = ["Hugo_Symbol", "Chromosome", "Start_position", "End_position", "Strand", "Variant_Classification", "Variant_Type", "Reference_Allele", "Tumor_Seq_Allele1", "Tumor_Seq_Allele2", "dbSNP_RS",
+                        "dbSNP_Val_Status", "Project_Code", "Donor_ID"] ### minimal required columns
+    checkval = []
+    for column in gdc_format_header:
+        if column in maf_totest.columns:
+            checkval.append(True)
+        else:
+            checkval.append(False)
+    format_test = all(checkval)
+    return format_test
+
+### Function to convert MAF input format to SigProfiler input MAF convention, hard-coded
+def mafconverter(maf_toconvert, ref_version):
+    maf_out_raw = pd.DataFrame(columns=["Hugo", "Entrez", "Center", "Genome", "Chrom", "Start", "End", "Strand", "Classification", "Type", "Ref", "Alt1", "Alt2", "dbSNP", "SNP_Val_status", "Tumor_sample"])
+    maf_out_raw["Hugo"] = maf_toconvert["Hugo_Symbol"]
+    maf_out_raw["Entrez"] = "."
+    maf_out_raw["Center"] = "ICGC"
+    maf_out_raw["Genome"] = ref_version
+    maf_out_raw["Chrom"] = maf_toconvert["Chromosome"]
+    maf_out_raw["Start"] = maf_toconvert["Start_position"]
+    maf_out_raw["End"] = maf_toconvert["End_position"]
+    maf_out_raw["Strand"] = maf_toconvert["Strand"]
+    maf_out_raw["Classification"] = maf_toconvert["Variant_Classification"]
+    maf_out_raw["Type"] = maf_toconvert["Variant_Type"]
+    maf_out_raw["Ref"] = maf_toconvert["Reference_Allele"]
+    maf_out_raw["Alt1"] = maf_toconvert["Tumor_Seq_Allele1"]
+    maf_out_raw["Alt2"] = maf_toconvert["Tumor_Seq_Allele2"]
+    maf_out_raw["dbSNP"] = maf_toconvert["dbSNP_RS"]
+    maf_out_raw["SNP_Val_status"] = maf_toconvert["dbSNP_Val_Status"]
+    maf_out_raw["Tumor_sample"] = maf_toconvert[['Project_Code', 'Donor_ID']].apply(lambda x: '_'.join(x), axis=1)
+    return maf_out_raw
+
+###
+def maf_input_routine(in_maf, ref_version):
+        maf_raw = pd.read_table(in_maf) ### expects MAF format following GDC protected data structure
+        format_conform = maf_formattest(maf_raw)
+        if format_conform == True:
+            converted_maf = mafconverter(maf_raw, ref_version)
+            return converted_maf
+        else:
+            raise ValueError(f"The provided MAF file {maf_raw} does not follow GDC format requirements. Please recheck your input MAF.")
+
+logger = logging.getLogger()
+
+############################################################################################################
+'''
+Define the signature assignment routine
+'''
+
+def sigpro_func_alt(sample, output_pattern, filetype, ref, context, signature_database):
+    sig.cosmic_fit(samples=sample, output=output_pattern, input_type=filetype, genome_build=ref, context_type=context, signature_database=signature_database)
+
+############################################################################################################
+
+def main(argv=None):
+    args = parse_args(argv)
+    if args.filetype in ["maf", "MAF"]:
+        if os.path.isfile(args.input):
+            maf_for_analysis = maf_input_routine(args.input, args.ref)
+            os.mkdir('assignment')
+            maf_for_analysis.to_csv('./assignment/' + args.output_pattern + '.maf', index = False, sep="\t")
+            '''Install reference genome'''
+            genInstall.install(args.ref, bash=True)
+            '''Run the Matrix Generator Module to generate matrices for SBS96 from input data'''
+            sigpro_func_alt("./assignment", args.output_pattern, "vcf", args.ref, args.context, args.signature_database)
+            shutil.move('./' + args.output_pattern, './output')
+        else:
+            raise ValueError(f"The given input MAF file {args.input} was not found!")
+    elif args.filetype in ["vcf", "VCF"]:
+        if os.path.isdir(args.input):
+            os.mkdir('assignment')
+            for file in glob.glob(args.input + '/*.vcf'):
+                shutil.copy(file, './assignment')
+            '''Install reference genome'''
+            genInstall.install(args.ref, bash=True)
+            '''Run the Matrix Generator Module to generate matrices for SBS96 from input data'''
+            sigpro_func_alt("./assignment", args.output_pattern, "vcf", args.ref, args.context, args.signature_database)
+            shutil.move('./' + args.output_pattern, './output')
+        else:
+            logger.error(f"The given temporary folder {args.input} was not found!")
+            raise ValueError(f"The given temporary folder {args.input} was not found!")
+    elif args.filetype in ['matrix', 'Matrix', 'counts', 'Counts']:
+        if os.path.isfile(args.input):
+            os.mkdir('assignment')
+            mutcount_matrix = pd.read_csv(args.input, index_col=0, sep="\t")
+            '''Install reference genome'''
+            genInstall.install(args.ref, bash=True)
+            '''Run the Matrix Generator Module to generate matrices for SBS96 from input data'''
+            sigpro_func_alt(mutcount_matrix, args.output_pattern, "matrix", args.ref, args.context, args.signature_database)
+            shutil.move('./' + args.output_pattern, './output')
+    else:
+        raise ValueError(f"The provided information for the input file type is wrong. Please define either 'vcf', 'matrix' or 'maf'!")
+
+if __name__ == '__main__':
+    sys.exit(main())
---
+++ modules/local/sigprofiler/assignment_alt/main.nf
@@ -0,0 +1,42 @@
+process ASSIGNMENT_ALT {
+    label 'process_high'
+
+    conda "bioconda::sigmut=1.0"
+    container "${ workflow.containerEngine == 'singularity' && !task.ext.singularity_pull_docker_container ?
+        'docker.io/fauzul/sigprofiler:1.0':
+        'docker.io/fauzul/sigprofiler:1.0' }"
+
+    input:
+    tuple val(meta), path(input)
+    path signature_catalogue
+    val  filetype
+    val  matgen_finished
+
+    output:
+    tuple val(meta), path("output")                   , emit: sigprofiler_output
+    path "versions.yml"                               , emit: versions
+
+    when:
+    task.ext.when == null || task.ext.when
+
+    script:
+    def args = task.ext.args ?: ''
+    def processdir = "\${PWD}"
+
+    """
+    sigprofiler_alt.py \\
+        --filetype $filetype \\
+        --input $input \\
+        --output_pattern $meta.id \\
+        --signature_database $signature_catalogue \\
+        $args \\
+        2> $processdir/sigprofiler.error.log \\
+        1> $processdir/sigprofiler.log
+
+    cat <<-END_VERSIONS > versions.yml
+    "${task.process}":
+        python: \$(python --version | sed 's/Python //g')
+        SigProfilerAssignment: \$(python -c "import SigProfilerAssignment; print(SigProfilerAssignment.__version__)")
+    END_VERSIONS
+    """
+}
